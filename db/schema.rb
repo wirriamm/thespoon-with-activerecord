5b20edfcb772c517aeedd4025bf2387a9c4ab8e4
William Goh <wirriamm@gmail.com>
2020-05-14 00:44:52 +0800

    Addesed address to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_162930) do
+ActiveRecord::Schema.define(version: 2020_05_13_163435) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
 end
