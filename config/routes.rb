da8652da096f3d69b30dde228531697238644e10
William Goh <wirriamm@gmail.com>
2020-05-14 15:52:57 +0800

    completed lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  # get 'restaurants/', to: 'restaurants#index'
+
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  resources :restaurants
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    rating: (1..5).to_a.sample
+    )
+end
